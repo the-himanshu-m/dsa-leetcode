997279b8afe3a7946400edcbd18e46902b7acffe
the-himanshu-m <thehimanshum123@gmail.com>
2026-07-20 21:40:00 +0530

    Time: 8 ms (92.14%), Space: 49.1 MB (42.99%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,13 +1,16 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 
 class Solution {
     public int[][] merge(int[][] arr) {
+
+        if (arr.length == 0) return new int[0][];
 
         Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
 
         int start = arr[0][0];
         int end = arr[0][1];
-        ArrayList<int[]> ans = new ArrayList<int[]>();
+        ArrayList<int[]> ans = new ArrayList<>();
 
         for (int i = 1; i < arr.length; i++) {
             if (end >= arr[i][0]) {
