648ccf8e18d54353773c8a3e81e9ae2ca4e894bf
the-himanshu-m <thehimanshum123@gmail.com>
2026-07-27 20:34:49 +0530

    Time: 1 ms (29.92%), Space: 47.2 MB (66.37%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
         sort(nums);
-        
+
         for (int i = 0; i < nums.length; i++) {
             if (i != nums[i]) return i;
         }
@@ -13,13 +13,18 @@ class Solution {
         int start = 0;
         while (start < nums.length) {
             int correct = nums[start];
-            if (correct != nums.length && nums[correct] != nums[start]) {
-                int temp = nums[start];
-                nums[start] = nums[correct];
-                nums[temp] = temp;
+
+            if (correct < nums.length && nums[correct] != nums[start]) {
+                swap(nums, correct, start);
             } else {
                 start++;
             }
         }
+    }
+
+    private void swap (int[] nums, int correct, int start) {
+        int temp = nums[correct];
+        nums[correct] = nums[start];
+        nums[start] = temp;
     }
 }
