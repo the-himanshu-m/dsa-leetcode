402146ff3289cdac1d45647c9140c6899a7bd525
the-himanshu-m <thehimanshum123@gmail.com>
2026-07-28 19:00:19 +0530

    Time: 8 ms (91.62%), Space: 48.9 MB (85.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -8,22 +8,22 @@ class Solution {
 
         Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
 
-        int start = arr[0][0];
-        int end = arr[0][1];
+        int first = arr[0][0];
+        int sec = arr[0][1];
         ArrayList<int[]> ans = new ArrayList<>();
 
         for (int i = 1; i < arr.length; i++) {
-            if (end >= arr[i][0]) {
-                end = Math.max(end, arr[i][1]);
+            if (sec >= arr[i][0]) {
+                sec = Math.max(sec, arr[i][1]);
             } else {
-                ans.add(new int[] {start, end});
-                start = arr[i][0];
-                end = arr[i][1];
+                ans.add(new int[]{first, sec});
+                first = arr[i][0];
+                sec = arr[i][1];
             }
         }
 
-        ans.add(new int[] {start, end});
+        ans.add(new int[]{first, sec});
 
         return ans.toArray(new int[ans.size()][]);
     }
-}
+} 
