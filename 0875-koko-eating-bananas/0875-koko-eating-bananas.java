005ec3db1fd499f39c38db4e538855a943148311
the-himanshu-m <thehimanshum123@gmail.com>
2026-08-15 14:19:55 +0530

    Time: 21 ms (29.11%), Space: 48.1 MB (32.88%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -7,19 +7,17 @@ class Solution {
         }
 
         int start = 1, end = largest;
-        int ans = 0;
         while (start <= end) {
             int mid = start + (end - start)/2;
 
             if (kokoCanDoIt(piles, mid, h)) {
-                ans = mid;
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
 
-        return ans;
+        return start;
     }
 
     private boolean kokoCanDoIt (int[] piles, int speed, int h) {
