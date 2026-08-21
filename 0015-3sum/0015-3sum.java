3799707ebed3243569a4ae6746383b3705dc394e
the-himanshu-m <thehimanshum123@gmail.com>
2026-08-21 22:08:02 +0530

    Time: 28 ms (96.84%), Space: 59.2 MB (60.02%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,36 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // Use a Set of Lists to automatically filter out duplicate triplets
-        Set<List<Integer>> res = new HashSet<>();
-        // Keeps track of elements we have already processed as the first element 'nums[i]'
-        Set<Integer> dups1 = new HashSet<>();
-        
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            // Skip if we have already looked for pairs using this exact starting number
-            if (!dups1.add(nums[i])) continue;
-            
-            // This inner set acts as our 2Sum lookup map for the remaining elements
-            Set<Integer> seen = new HashSet<>();
-            
-            for (int j = i + 1; j < nums.length; j++) {
-                int complement = -nums[i] - nums[j];
-                
-                if (seen.contains(complement)) {
-                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
-                    // Sorting the individual triplet is mandatory so the HashSet 
-                    // recognizes [-1, 0, 1] and [0, -1, 1] as identical items.
-                    Collections.sort(triplet);
-                    res.add(triplet);
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int start = i + 1, end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[start] + nums[end] + nums[i];
+
+                if (sum < 0) {
+                    start++;
+                } else if (sum > 0) {
+                    end--;
+                } else {
+                    ans.add(Arrays.asList(nums[start], nums[end], nums[i]));
+
+                    while (start < end && nums[start] == nums[start + 1]) {
+                        start++;
+                    }
+
+                    while (end > start && nums[end] == nums[end - 1]) {
+                        end--;
+                    }
+
+                    start++;
+                    end--;
                 }
-                seen.add(nums[j]);
             }
         }
-        return new ArrayList<>(res);
+
+        return ans;
     }
 }
