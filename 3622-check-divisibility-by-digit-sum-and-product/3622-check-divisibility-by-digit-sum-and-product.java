b993627d7f14743d0ae7ecfa7c147b117e3064bf
the-himanshu-m <thehimanshum123@gmail.com>
2026-08-22 22:19:56 +0530

    Time: 1 ms (25.54%), Space: 42.2 MB (91.23%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -4,7 +4,7 @@ class Solution {
     }
 
     private int digitSum (int n) {
-        if (n < 10) return n;
+        // if (n < 10) return n;
         int sum = 0;
         while (n != 0) {
             int rem = n % 10;
