a387655b140a16dcc298a900da2c433777a35652
the-himanshu-m <thehimanshum123@gmail.com>
2026-08-29 21:38:19 +0530

    Time: 1 ms (94.99%), Space: 44.2 MB (50.55%) - LeetHub

---
+++ 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minBishopMoves(int[] source, int[] target) {
+        if (source[0] == target[0] && source[1] == target[1]) {
+            return 0;
+        }
+
+        if ((source[0] + source[1]) % 2 != (target[0] + target[1]) % 2) {
+            return -1;
+        }
+
+        if (Math.abs(source[0] - target[0]) == Math.abs(source[1] - target[1])) {
+            return 1;
+        }
+
+        return 2;
+    }
+}
