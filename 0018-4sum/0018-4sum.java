fddd0fd97a5cbdcfb59e70bc2e556eea48b38b97
the-himanshu-m <thehimanshum123@gmail.com>
2026-09-02 13:34:08 +0530

    Update 0018-4sum.java

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            for (int j = i + 1; j < nums.length - 1; j++) {
+            for (int j =i + 1; j < nums.length - 1; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
                 int start = j + 1, end = nums.length - 1;
