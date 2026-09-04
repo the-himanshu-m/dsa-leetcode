cc767fa81f92239d389df34db786fb12d194f735
the-himanshu-m <thehimanshum123@gmail.com>
2026-09-04 20:28:58 +0530

    Update 0007-reverse-integer.java

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -10,7 +10,7 @@ class Solution {
             rev = (rev * 10) + rem;
         }
 
-        if (rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE) return 0;
+        if(rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE) return 0;
 
         return (int)rev;
     }
