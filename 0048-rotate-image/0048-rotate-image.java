1b257d73e2558bad98f81b9b592203373696df95
the-himanshu-m <thehimanshum123@gmail.com>
2026-09-07 11:40:42 +0530

    Time: 0 ms (100%), Space: 43.8 MB (52.84%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,32 +1,25 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        for (int row = 0; row < matrix[0].length; row++) {
+        for (int row = 0; row < matrix.length; row++) {
             for (int col = row + 1; col < matrix[0].length; col++) {
-                swap(matrix, row, col);
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = temp;
             }
         }
 
-        for (int row = 0; row < matrix[0].length; row++) {
-            reverse(matrix, row, matrix[row].length - 1);
-        }
-    }
+        for (int i = 0; i < matrix.length; i++) {
+            int start = 0;
+            int end = matrix[i].length - 1;
 
-    private void swap (int[][]matrix, int row, int col) {
-        int temp = matrix[row][col];
-        matrix[row][col] = matrix[col][row];
-        matrix[col][row] = temp;
-    }
-
-    private void reverse (int[][] matrix, int row, int end) {
-        int start = 0;
-
-        while (start < end) {
-            int temp = matrix[row][end];
-            matrix[row][end] = matrix[row][start];
-            matrix[row][start] = temp;
-
-            start++;
-            end--;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                
+                start++;
+                end--;
+            }
         }
     }
 }
