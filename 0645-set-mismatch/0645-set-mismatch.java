9a85a214af2dbf1a7c391143a01c5868370ba740
the-himanshu-m <thehimanshum123@gmail.com>
2026-09-08 17:40:07 +0530

    Time: 5 ms (42.67%), Space: 47.5 MB (63.05%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -2,14 +2,12 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         sort(nums);
 
-        int[] ans = new int[2];
+        int ans[] = new int[2];
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != i + 1) {
                 ans[0] = nums[i];
                 ans[1] = i + 1;
-
-                return ans;
             }
         }
 
