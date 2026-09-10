9d0558269a0a9c5b1faddb8e856eb5e4245df3ba
the-himanshu-m <thehimanshum123@gmail.com>
2026-09-10 16:01:22 +0530

    Time: 0 ms (100%), Space: 43.8 MB (65.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,18 +5,19 @@ class Solution {
         while (start <= end) {
             int mid = start + (end - start)/2;
 
-            if (nums[mid] == target) return mid;
-            if (nums[mid] >= nums[start]) {
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            } else {
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] <= nums[end]) {
                 if (target > nums[mid] && target <= nums[end]) {
                     start = mid + 1;
                 } else {
                     end = mid - 1;
+                }
+            } else {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
             }
         }
